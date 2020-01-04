31940d76f17cc94a21a83337122b347cc009d941
MSBR <michasam.raredon@yale.edu>
2020-01-04 14:34:49 -0500

    NetworkPlot initial

--- R/NetworkPlot.r
+++ R/NetworkPlot.r
@@ -1,24 +1,16 @@
-#' NetworksCompare
+#' NetworkPlot
 #'
-#' This function takes a list of connectomes and plots the networks side by side. Performs on a mode of interest (MOI)
-#' Output will be an image
-#' @param con.list A list of connectomes
-#' @param MOI The specific mode of interest to plot
+#' Creates a network 'hairball' plot of a filtered object.  Wrapper for igraph functionality.
+#'
+#' @param connectome A connectomic object, ideally filtered to only edges of interest.
+#' @param weight.attribute The desired column to use for edgeweights. Defaults to 'weight_sc'
 #' @export
 
-NetworksCompare <- function(con.list,MOI,weight.attribute = 'weight_sc',...){
-  require(igraph)
-  require(RColorBrewer)
-  require(scales)
-  pdf(file = paste(MOI,"Networks Compare.pdf",sep=" "),width = 11,height = 2)
-  par(mfrow=c(1,length(con.list)))
-  par(mar = c(0,0,1,0))
-  par(cex.main = 1)
+NetworkPlot <- function(connectome, weight.attribute = 'weight_sc',title = NULL,...){
 
-  for (j in 1:length(con.list)){
-    con <- con.list[[j]]
+    con <- connectome
     nodes <- sort(unique(union(con$source,con$target)))
-    edgelist <- subset(con,con$mode == MOI)
+    edgelist <- con
     net <- graph_from_data_frame(d = edgelist, vertices = nodes, directed = T)
     lay <- layout_in_circle(net)
       V(net)$color <- hue_pal()(length(nodes))
@@ -32,10 +24,9 @@ NetworksCompare <- function(con.list,MOI,weight.attribute = 'weight_sc',...){
                   c.rotate(scales::rescale(x, c(0, 2 * pi), range(x)))
                 }
 
-            V(net)$label.degree <- radian.rescale(x=1:length(nodes), direction=-1, start=0)
-            V(net)$label.family <- "Helvetica"
-            V(net)$label.cex <- 0.6
-
+      V(net)$label.degree <- radian.rescale(x=1:length(nodes), direction=-1, start=0)
+      V(net)$label.family <- "Helvetica"
+      V(net)$label.cex <- 0.6
 
       E(net)$weight <- get.edge.attribute(net, weight.attribute)
       E(net)$color <- "grey"
@@ -43,12 +34,16 @@ NetworksCompare <- function(con.list,MOI,weight.attribute = 'weight_sc',...){
       try(E(net)$color <- rbPal(10)[as.numeric(cut(E(net)$weight,breaks = 10))])
       E(net)$arrow.size <- 0.5
       E(net)$width <- 1
-
+    if (!is.null(title)){
       plot(net, layout=lay,
         edge.label=E(net)$pair, edge.label.family="Helvetica", edge.label.cex=0.4,
         edge.label.color = "black",
-        main=paste(MOI,"Network",names(con.list)[j],sep = ' '),
+        main=paste(title,"Network",sep = ' '),
         vertex.label.color = "black")
-    }
-    dev.off()
+    }else{
+      plot(net, layout=lay,
+        edge.label=E(net)$pair, edge.label.family="Helvetica", edge.label.cex=0.4,
+        edge.label.color = "black",
+        vertex.label.color = "black")
+      }
 }
